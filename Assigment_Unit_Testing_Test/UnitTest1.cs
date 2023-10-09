d7714a9bff12aed8e7599de6a1d6fc539168826d
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-10-08 19:01:30 -0700

    Add test framwork

---
+++ Assigment_Unit_Testing_Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Assigment_Unit_Testing_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
